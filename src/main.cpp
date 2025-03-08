2c66c686216d0a90b21a61c1b496fed880516de2
AmirrezaDaei <amir2006daei@gmail.com>
2025-03-08 14:52:57 +0330

    Define structs

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct student {
+    string name;
+    int ID;
+    int friendID;
+    int enemyID;
+};
+
+struct table {
+    int ID;
+    vector<int> coordination;
+    int capacity;
+    int bonus;
+    vector<student> students;
+    vector<student> queue;
+    int score;
+};
+
 int main() {
     return 0;
 }
