63f725331f0ba1429af16d744f352fb8bc4ce2d3
AmirrezaDaei <amir2006daei@gmail.com>
2025-03-09 00:57:27 +0330

    Write readTables function

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,17 @@
 
 using namespace std;
 
+const string WINDOW = "window";
+const string DOOR = "door";
+const string MIDDLE = "middle";
+
 struct table {
     int ID;
+    int x;
+    int y;
     int capacity;
     int bonus;
     int score = 0;
-    int x;
-    int y;
     vector<struct student> students;
     vector<struct student> queue;
 };
@@ -37,16 +41,33 @@ vector<string> readFile(string fileAddress) {
             datas.push_back(value);
         }
     }
+    file.close();
     return datas;
 }
 
-void readTables(vector<string> tables) {
-    
+void readTables(vector<string> tablesData, vector<table> &tables) {
+    for(int i = 0; i < tablesData.size()/5; i++) {
+        struct table newTable;
+        newTable.ID = stoi(tablesData[i * 5]);
+        newTable.x = stoi(tablesData[i * 5 + 1]);
+        newTable.y = stoi(tablesData[i * 5 + 2]);
+        newTable.capacity = stoi(tablesData[i * 5 + 3]);
+        if(tablesData[i * 5 + 4] == WINDOW)
+            newTable.bonus = 6;
+        else if(tablesData[i * 5 + 4] == DOOR)
+            newTable.bonus = 4;
+        else if(tablesData[i * 5 + 4] == MIDDLE)
+            newTable.bonus = 2;
+        tables.push_back(newTable);
+    }
 }
+
+
 
 int main(int argc,char* argv[]) {
     vector<table> tables;
     vector<student> students;
     vector<string> tablesData = readFile(argv[1]);
     vector<string> studentsData = readFile(argv[2]);
+    readTables(tablesData, tables);
 }
