23c5e12d0c091f4ad62ede205d613e4c6b866882
AmirrezaDaei <amir2006daei@gmail.com>
2025-03-08 16:16:16 +0330

    Update structs

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
+#include <string>
 #include <vector>
+
 using namespace std;
+
+struct table {
+    int ID;
+    int capacity;
+    int bonus;
+    int score = 0;
+    vector<int> coordination;
+    vector<struct student> students;
+    vector<struct student> queue;
+};
 
 struct student {
     string name;
     int ID;
     int friendID;
     int enemyID;
+    struct table table = NULL;
 };
 
-struct table {
-    int ID;
-    vector<int> coordination;
-    int capacity;
-    int bonus;
-    vector<student> students;
-    vector<student> queue;
-    int score;
+
+struct system{
+    vector<struct table> tables;
+    vector<struct student> students;
 };
 
 int main() {
